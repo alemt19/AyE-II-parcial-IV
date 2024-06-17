08cada0a19c4bd56fb0c7b2622c405bdaf69f6f2
EmilioUzcategui <167447158+EmilioUzcategui@users.noreply.github.com>
2024-06-17 14:51:01 -0400

    cambios menores

--- proyecto4/backup.py
+++ proyecto4/backup.py
@@ -11,7 +11,6 @@ def cargar_datos_desde_json():
     with open(config_path, "r") as config_file:
         config = json.load(config_file)
     ruta_proyectos = config["datos"]
-    ruta_subtareas = config["subtareas"]
     
     # Cargar datos de proyectos
     with open(ruta_proyectos, "r") as archivo:
@@ -29,35 +28,20 @@ def cargar_datos_desde_json():
             )
             
             proyecto.id = proyecto_data["id"]  # Asigna el ID desde los datos cargados
-            
+            for i in proyecto_data["tareas"]:
+                proyecto.tareas.append(tr.Tareas(i["id"],
+                                       i["nombre"],
+                                       i["descripcion"],
+                                       i["fecha_inicio"],
+                                       i["fecha_vencimiento"],
+                                       i["estado"],
+                                       i["empresa"],
+                                       i["cliente"],
+                                       i["porcentaje"],
+                                       i["subtareas"]))
             proyectos.append(proyecto)
     
     # Cargar subtareas desde subtareas.json
-    with open(ruta_subtareas, "r") as subtareas_archivo:
-        subtareas_datos = json.load(subtareas_archivo)
-        for proyecto in proyectos:
-            proyecto_id = proyecto.id
-            for tarea in subtareas_datos.get(str(proyecto_id), []):
-                tarea_obj = tr.Tareas(
-                    tarea["id"],
-                    tarea["nombre"],
-                    "",  # Cliente (si es necesario añadirlo)
-                    tarea["descripcion"],
-                    datetime.strptime("1999-12-12", "%Y-%m-%d"),
-                    datetime.strptime(tarea["fecha_vencimiento"], "%Y-%m-%d"),
-                    "completado",
-                    0  # Avance (si es necesario añadirlo)
-                )
-                
-                for subtarea_data in tarea.get("subtareas", []):
-                    subtarea_obj = Subtarea( # type: ignore
-                        subtarea_data["id"],
-                        subtarea_data["nombre"],
-                    )
-                    tarea_obj.agregar_subtarea(subtarea_obj)
-                
-                proyecto.tareas.append(tarea_obj)
-    
     # Respaldar datos de proyectos a un archivo
     with open(ruta_proyectos, "w") as respaldo_proyectos:
         json.dump(datos, respaldo_proyectos, indent=4)
